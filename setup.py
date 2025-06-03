948761736525e7bfca83b849ab43f4fc0a640217
DingHongyu <38337171+micahvista@users.noreply.github.com>
2025-06-03 21:21:11 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -10,14 +10,6 @@ import numpy as np
 
 sys.path.append('python')
 
-extra_compile_args = ['-DHAVE_KALLOC']
-include_dirs = [".", np.get_include()]
-
-if platform.machine() in ["aarch64", "arm64"]:
-	include_dirs.append("sse2neon/")
-	extra_compile_args.extend(['-ftree-vectorize', '-DKSW_SSE2_ONLY', '-D__SSE2__'])
-else:
-	extra_compile_args.append('-msse4.1') # WARNING: ancient x86_64 CPUs don't have SSE4
 
 
 
@@ -33,16 +25,6 @@ setup(
 	keywords = 'sequence-alignment',
 	packages = find_packages("src"),
 	package_dir = {"": "src"},
-	ext_modules = [Extension('vacmap_index',
-		sources = ['index/vacmap_index.pyx', 'index/bseq.c', 'index/seed.c', 'index/sketch.c', 'index/index.c', 'index/options.c',
-				   'index/ksw2_extd2_sse.c', 'index/ksw2_exts2_sse.c', 'index/ksw2_extz2_sse.c', 'index/ksw2_ll_sse.c',
-				   'index/kalloc.c', 'index/kthread.c', 'index/map.c', 'index/misc.c', 'index/sdust.c', 'index/esterr.c', 'index/splitidx.c'],
-		depends = ['index/minimap.h', 'index/bseq.h', 'index/kalloc.h', 'index/kdq.h', 'index/khash.h', 'index/kseq.h', 'index/ksort.h',
-				   'index/ksw2.h', 'index/kthread.h', 'index/kvec.h', 'index/mmpriv.h', 'index/sdust.h',
-				   'index/cmappy.h', 'index/cmappy.pxd'],
-		extra_compile_args = extra_compile_args,
-		include_dirs = include_dirs,
-		libraries = ['z', 'm', 'pthread'])],
 	classifiers = [
 		'Development Status :: 5 - Production/Stable',
 		'License :: OSI Approved :: MIT License',
