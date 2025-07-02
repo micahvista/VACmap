3c7379fc2ddae6a453dee7d6b25e5cefef1ef56d
DingHongyu <38337171+micahvista@users.noreply.github.com>
2025-07-02 08:57:56 +0800

    Update mammap_noprefercloser.py

--- src/vacmap/mammap_noprefercloser.py
+++ src/vacmap/mammap_noprefercloser.py
@@ -1,9 +1,18 @@
-'''   
- * @Title: VACmap 
- * @author: Ding Hongyu
- * @date: 17 6 2024
- * @version V1.0.0
-'''
+# VACmap - a long-read aligner for structural variation discovery
+# Copyright (C) 2023 Hongyu Ding
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import edlib
 
 import pandas as pd
